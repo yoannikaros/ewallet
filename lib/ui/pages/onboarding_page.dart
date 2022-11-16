db671b13ccfe19e093018ad9649a3d03e24658df
yoannikaros <yoannikaros29@gmail.com>
2022-11-16 19:41:07 +0700

    Sign In Page

---
+++ lib/ui/pages/onboarding_page.dart
@@ -0,0 +1,201 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ewallet/shared/theme.dart';
+import 'package:ewallet/ui/pages/sign_in_page.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingPage extends StatefulWidget {
+  const OnBoardingPage({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardingPage> createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  int currenIndex = 0;
+  CarouselController carouselController = CarouselController();
+
+  List<String> titles = [
+    'Grow Your \n Financial Today',
+    'Build From \n Zero to Freedom',
+    'Start Together'
+  ];
+
+  List<String> subtitles = [
+    'Our system is helping you to \n achive a better goal',
+    'We Provide tips for you so that \n you can adapt easier',
+    'We will guide you to where \n you wanted it too'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CarouselSlider(
+              items: [
+                Image.asset(
+                  'assets/img_onboarding1.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding2.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding3.png',
+                  height: 331,
+                ),
+              ],
+              options: CarouselOptions(
+                height: 331,
+                viewportFraction: 1,
+                enableInfiniteScroll: false,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    currenIndex = index;
+                  });
+                },
+              ),
+              carouselController: carouselController,
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 24),
+              padding: EdgeInsets.symmetric(horizontal: 22, vertical: 24),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    titles[currenIndex],
+                    style: blackTextStyle.copyWith(
+                        fontSize: 20, fontWeight: semiBold),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 26,
+                  ),
+                  Text(
+                    subtitles[currenIndex],
+                    style: greyTextStyle.copyWith(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: currenIndex == 2 ? 38 : 50,
+                  ),
+                  currenIndex == 2
+                      ? Column(
+                          children: [
+                            SizedBox(
+                              height: 50,
+                              width: double.infinity,
+                              child: TextButton(
+                                onPressed: () {
+                                  carouselController.nextPage();
+                                },
+                                style: TextButton.styleFrom(
+                                    backgroundColor: purpleColor,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(56))),
+                                child: Text(
+                                  'Get Started',
+                                  style: whiteTextStyle.copyWith(
+                                      fontSize: 16, fontWeight: semiBold),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            SizedBox(
+                              height: 50,
+                              width: double.infinity,
+                              child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const SignInPage()));
+                                },
+                                style: TextButton.styleFrom(
+                                    padding: EdgeInsets.zero),
+                                child: Text(
+                                  'Sign In',
+                                  style: greyTextStyle.copyWith(fontSize: 16),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      : Row(
+                          children: [
+                            Container(
+                              height: 12,
+                              width: 12,
+                              margin: EdgeInsets.only(right: 10),
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: currenIndex == 0
+                                      ? blueColor
+                                      : lightBackgroundColor),
+                            ),
+                            Container(
+                              height: 12,
+                              width: 12,
+                              margin: EdgeInsets.only(right: 10),
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: currenIndex == 1
+                                      ? blueColor
+                                      : lightBackgroundColor),
+                            ),
+                            Container(
+                              height: 12,
+                              width: 12,
+                              margin: EdgeInsets.only(right: 10),
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: currenIndex == 2
+                                      ? blueColor
+                                      : lightBackgroundColor),
+                            ),
+                            const Spacer(),
+                            SizedBox(
+                              height: 50,
+                              width: 200,
+                              child: TextButton(
+                                onPressed: () {
+                                  carouselController.nextPage();
+                                },
+                                style: TextButton.styleFrom(
+                                    backgroundColor: purpleColor,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(56))),
+                                child: Text(
+                                  'Continue',
+                                  style: whiteTextStyle.copyWith(
+                                      fontSize: 16, fontWeight: semiBold),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
