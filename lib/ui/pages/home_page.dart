9a7ccbccb75780f34b8837be5cad2c82d2270261
yoannikaros <yoannikaros29@gmail.com>
2022-11-20 20:51:49 +0700

    create home page

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -7,12 +7,109 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: blackColor,
-      body: Center(
-        child: Text(
-          'TESSSSS',
-          textAlign: TextAlign.center,
+      backgroundColor: lightBackgroundColor,
+      bottomNavigationBar: BottomAppBar(
+        color: whiteColor,
+        shape: CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6, //jarak antar buttom
+        elevation: 0,
+        child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed, //Merapihkan icon
+            elevation: 0, //menghilangkan garis hitam
+            backgroundColor: whiteColor,
+            selectedItemColor: blueColor,
+            unselectedItemColor: blackColor,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            selectedLabelStyle:
+                blueTextStyle.copyWith(fontSize: 10, fontWeight: medium),
+            unselectedLabelStyle:
+                blackTextStyle.copyWith(fontSize: 10, fontWeight: medium),
+            items: [
+              BottomNavigationBarItem(
+                  icon: Image.asset(
+                    'assets/ic_overview.png',
+                    width: 20,
+                    color: blueColor,
+                  ),
+                  label: 'Overview'),
+              BottomNavigationBarItem(
+                  icon: Image.asset('assets/ic_history.png', width: 20),
+                  label: 'history'),
+              BottomNavigationBarItem(
+                  icon: Image.asset('assets/ic_statistic.png', width: 20),
+                  label: 'Statistic'),
+              BottomNavigationBarItem(
+                  icon: Image.asset('assets/ic_reward.png', width: 20),
+                  label: 'reward'),
+            ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: purpleColor,
+        onPressed: () {},
+        child: Image.asset(
+          'assets/ic_plus_circle.png',
+          width: 24,
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [BuildProfile()],
+      ),
+    );
+  }
+
+  Widget BuildProfile() {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment
+            .spaceBetween, //Pisahkan Component antara kiri dan kanan
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Howdy',
+                style: greyTextStyle.copyWith(fontSize: 16),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text(
+                'Yoan',
+                style:
+                    blackTextStyle.copyWith(fontSize: 20, fontWeight: semiBold),
+              ),
+            ],
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: AssetImage('assets/img_profile.png'))),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: whiteColor),
+                child: Center(
+                  child: Icon(
+                    Icons.check_circle,
+                    color: greenColor,
+                    size: 14,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
